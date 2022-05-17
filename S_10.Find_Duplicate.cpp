d3da76711ba3119f7794be181bb588bc22f31036
a0yan <76205444+a0yan@users.noreply.github.com>
2022-05-17 12:45:32 +0530

    Create S_10.Find_Duplicate.cpp

---
+++ S_10.Find_Duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+     for(int i=0;i<n;i++) {
+         int val=abs(nums[i])-1;
+         if(nums[val]<0){
+             return val+1;
+         }
+         else{
+             nums[val]=-nums[val];
+         }
+     }
+        return 0;
+    }
+};
