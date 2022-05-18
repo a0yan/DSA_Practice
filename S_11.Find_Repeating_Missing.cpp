d358c7e5afb6729f58f948fdead2399be434855b
a0yan <76205444+a0yan@users.noreply.github.com>
2022-05-18 11:02:29 +0530

    Create S_11.Find_Repeating_Missing.cpp

---
+++ S_11.Find_Repeating_Missing.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &A, int n)
+{
+ long long int len = A.size();
+
+    long long int S = (len * (len+1) ) /2;
+    long long int P = (len * (len +1) *(2*len +1) )/6;
+    long long int missingNumber=0, repeating=0;
+     
+    for(int i=0;i<A.size(); i++){
+       S -= (long long int)A[i];
+       P -= (long long int)A[i]*(long long int)A[i];
+    }
+     
+    missingNumber = (S + P/S)/2;
+
+    repeating = missingNumber - S;
+
+    pair <int,int> ans;
+
+    ans.first=missingNumber;
+    ans.second=repeating;
+
+    return ans;
+}
