0669c4b625c685e761352a3c935bba685288caa0
a0yan <76205444+a0yan@users.noreply.github.com>
2022-05-19 11:27:47 +0530

    Update S_10.Find_Duplicate.cpp

--- S_10.Find_Duplicate.cpp
+++ S_10.Find_Duplicate.cpp
@@ -13,4 +13,20 @@ public:
      }
         return 0;
     }
-};/
+};
+------------------Without Modifying Array------------------
+------------------Cycle Detection Algorithm--------------------------
+ int findDuplicate(vector < int > & nums) {
+  int slow = nums[0];
+  int fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+  fast = nums[0];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
