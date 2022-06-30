2cc29f0b145fd144a74bdc3e16b3994442c7d486
a0yan <76205444+a0yan@users.noreply.github.com>
2022-06-30 13:54:21 +0530

    Create A_33.Valid_Palindrome_ii.cpp

---
+++ A_33.Valid_Palindrome_ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+bool isValid(string s, int i, int j){
+    while(i<j){
+        if(s[i]!=s[j])return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+
+bool validPalindrome(string s) {
+    int i=0, j=s.size()-1;
+    while(i<j && s[i]==s[j]){
+        i++;
+        j--;
+    }
+    return isValid(s,i,j-1) || isValid(s,i+1,j); 
+}
+};
